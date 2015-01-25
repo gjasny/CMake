a89b4e281a08780c45b2173ca1d5b8bb346cea3a
Gregor Jasny <gjasny@googlemail.com>
2015-01-25 19:23:53 +0100

    Add handling for XCTest bundles
    An XCTest bundle is a CFBundle with a special product-type and bundle
    extension. For more information about XCTest visit the Mac
    Developer library at:
    
      http://developer.apple.com/library/mac/documentation/DeveloperTools/Conceptual/testing_with_xcode/
    
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

---
+++ Help/release/dev/xcode-xctest.rst
@@ -0,0 +1,6 @@
+Xcode XCTest Support
+--------------------
+
+* CMake gained the abiltity to create XCTest bundles to test
+  Frameworks and App Bundles within Xcode. The :module:`XCTestUtilities`
+  module provides convenience functions to handle :prop_tgt:`XCTEST` bundles.
--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -615,6 +615,13 @@ bool cmTarget::IsCFBundleOnApple() const
 }
 
 //----------------------------------------------------------------------------
+bool cmTarget::IsXCTestOnApple() const
+{
+  return (this->IsCFBundleOnApple() &&
+          this->GetPropertyAsBool("XCTEST"));
+}
+
+//----------------------------------------------------------------------------
 bool cmTarget::IsBundleOnApple() const
 {
   return this->IsFrameworkOnApple() || this->IsAppBundleOnApple() ||
@@ -6774,7 +6781,14 @@ std::string cmTarget::GetCFBundleDirectory(const std::string& config,
   const char *ext = this->GetProperty("BUNDLE_EXTENSION");
   if (!ext)
     {
-    ext = "bundle";
+    if (this->IsXCTestOnApple())
+      {
+      ext = "xctest";
+      }
+    else
+      {
+      ext = "bundle";
+      }
     }
   fpath += ext;
   fpath += "/Contents";
