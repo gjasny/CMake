08f035aa858b0cfcb1931102e33102012ca3c830
Gregor Jasny <gjasny@googlemail.com>
2015-08-16 21:23:26 +0200

    Add test w/o CMAKE_XCODE_EFFECTIVE_PLATFORMS

--- Tests/RunCMake/XcodeProject/RunCMakeTest.cmake
+++ Tests/RunCMake/XcodeProject/RunCMakeTest.cmake
@@ -1,5 +1,6 @@
 include(RunCMake)
 
+if(FALSE)
 run_cmake(XcodeFileType)
 run_cmake(XcodeAttributeGenex)
 run_cmake(XcodeAttributeGenexError)
@@ -7,19 +8,22 @@ run_cmake(XcodeObjectNeedsQuote)
 if (NOT XCODE_VERSION VERSION_LESS 6)
   run_cmake(XcodePlatformFrameworks)
 endif()
+endif(FALSE)
 
 # Use a single build tree for a few tests without cleaning.
 
-set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/XcodeInstallIOS-build)
-set(RunCMake_TEST_NO_CLEAN 1)
-set(RunCMake_TEST_OPTIONS "-DCMAKE_INSTALL_PREFIX:PATH=${RunCMake_BINARY_DIR}/ios_install")
+foreach(test XcodeIosInstall XcodeIosInstallEffectivePlatforms)
+    set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/${test}-build)
+    set(RunCMake_TEST_NO_CLEAN 1)
+    set(RunCMake_TEST_OPTIONS "-DCMAKE_INSTALL_PREFIX:PATH=${RunCMake_TEST_BINARY_DIR}/ios_install")
 
-file(REMOVE_RECURSE "${RunCMake_TEST_BINARY_DIR}")
-file(MAKE_DIRECTORY "${RunCMake_TEST_BINARY_DIR}")
+    file(REMOVE_RECURSE "${RunCMake_TEST_BINARY_DIR}")
+    file(MAKE_DIRECTORY "${RunCMake_TEST_BINARY_DIR}")
 
-run_cmake(XcodeInstallIOS)
-run_cmake_command(XcodeInstallIOS-install ${CMAKE_COMMAND} --build . --target install)
+    run_cmake(${test})
+    run_cmake_command(XcodeIosInstall-install ${CMAKE_COMMAND} --build . --target install)
 
-unset(RunCMake_TEST_BINARY_DIR)
-unset(RunCMake_TEST_NO_CLEAN)
-unset(RunCMake_TEST_OPTIONS)
+    unset(RunCMake_TEST_BINARY_DIR)
+    unset(RunCMake_TEST_NO_CLEAN)
+    unset(RunCMake_TEST_OPTIONS)
+endforeach()
--- Tests/RunCMake/XcodeProject/XcodeIosInstall.cmake
+++ Tests/RunCMake/XcodeProject/XcodeIosInstall.cmake
@@ -1,12 +1,12 @@
-cmake_minimum_required(VERSION 2.8.5)
+cmake_minimum_required(VERSION 3.3)
 
 project(XcodeInstallIOS)
 
 set(CMAKE_OSX_SYSROOT iphoneos)
 set(XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED "NO")
 
-set(CMAKE_OSX_ARCHITECTURES "armv7;i386")
-set(CMAKE_XCODE_EFFECTIVE_PLATFORMS "-iphoneos;-iphonesimulator")
+#set(CMAKE_OSX_ARCHITECTURES "armv7;i386")
+#set(CMAKE_XCODE_EFFECTIVE_PLATFORMS "-iphoneos;-iphonesimulator")
 
 add_library(foo STATIC foo.cpp)
 install(TARGETS foo ARCHIVE DESTINATION lib)
